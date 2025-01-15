da7a4020561f32ea53b8dc0a1f5dbdead080c95e
C0d3r090904 <archisman.basu0909@gmail.com>
2025-01-15 13:55:21 +0530

    Changes

--- pages/urls.py
+++ pages/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path("menu/", CustomMenuPageView.as_view(), name="menu"),
     path("course/add/", CreateCourseView.as_view(), name="create_course"),
     path("co/add/", CreateCOView.as_view(), name="create_co"),
+    path("AssesmentRubrics/", assesmentrubrics.as_view(), name="create_ar"),
 ]
